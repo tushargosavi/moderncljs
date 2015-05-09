e25795ed9d5123ea6b3a4291391afc064feceb36
Tushar Gosavi <tushar@datatorrent.com>
2015-05-09 22:28:39 +0530

    chapter 2 , works in firefox only

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   
   :source-paths ["src/clj", "src/cljs"]
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 ;;[org.clojure/clojurescript "0.0-2069"]]
                  [org.clojure/clojurescript "0.0-3211"]]
   
   :plugins [[lein-cljsbuild "1.0.5"]]
